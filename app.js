7d6b9d7dc0d84e4f2578b65194c23cafe206d51e
Filipe Neves <filipe.neves@redefrota.com.br>
2025-02-20 12:48:47 -0300

    Allowing cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { PORT } from './config/env.js';
+import { cors } from 'cors';
 
 const app = express();
 
 import presenceRouter from './src/routes/presence.routes.js';
 import connectToDatabase from './database/mongodb.js';
 
+app.use(cors());    
 app.use(express.json());
 
 app.use('/presence', presenceRouter);
