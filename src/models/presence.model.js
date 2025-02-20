35e7a364753dd5b18e5ed9c35581d4cae443dc57
Filipe Neves <filipe.neves@redefrota.com.br>
2025-02-20 15:48:01 -0300

    emails is not usable

--- src/models/presence.model.js
+++ src/models/presence.model.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 const presenceSchema = new mongoose.Schema({
     name: { type: String, required: [true, 'Name is required'] },
-    email: { type: String, required: [false], unique: false },
     date: { type: Date, default: Date.now }
 });
 
